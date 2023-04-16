22e44c8198f7a3b4f726345a73672573a571158e
FacundoPiaggio <fpiaggio@frba.utn.edu.ar>
2023-04-16 19:08:22 +0000

    Agrego funcion para calcular el estimado de la proxima rafaga de cpu.

--- kernel/include/kernelUtils.h
+++ kernel/include/kernelUtils.h
@@ -16,13 +16,50 @@ extern int socketMemoria;
 extern int socketCPU;
 extern int socketFS;
 
+/**
+* @NAME: agregar_socket_a_diccionario
+* @DESC: Dado un PID y un socket correspondiente a una consola, guarda el PID como key, del value socket.
+* @PARAMS:
+*        int pcb_pid - El pid que sera utilizado como key.
+*        int socket_consola - El socket que sera el value guardado en el diccionario.
+*/
 void agregar_socket_a_diccionario(int, int);
 
 // Esta funcion es solo para probar que se estan guardando bien los sockets.
 void leer_diccionario_consolas();
 
+/**
+* @NAME: calcular_response_ratio
+* @DESC: Dado un pcb* calcula el response ratio para el mismo.
+* @PARAMS:
+*        pcb* un_pcb - El proceso que se desea calcular el response ratio.
+*
+* @RETURN:
+*        Retorna el response ratio calculado.
+*/
 double calcular_response_ratio(pcb*);
 
+/**
+* @NAME: calcular_response_ratio
+* @DESC: Dados dos pcb, calcula si el primero tiene mayor response ratio que el segundo.
+* @PARAMS:
+*        pcb* un_pcb - El proceso que se desea ver si tiene mayor response ratio.
+*        pcb* otro_pcb - El otro proceso.
+* @RETURN:
+*        Retorna true o false.
+*/
 bool comparar_response_ratio(pcb*, pcb*);
+
+/**
+* @NAME: estimar_proxima_rafaga
+* @DESC: Dado un pcb* (QUE HAYA EJECUTADO) calcula una estimacion para la proxima rafaga de cpu.
+* @PARAMS:
+*        pcb* un_pcb - El proceso al que se desea calcular una estimacion de proxima rafaga.
+* @RETURN:
+*        Retorna el valor estimado.
+* @ACLARACION:
+*        EL PROCESO DEBE HABER EJECUTADO POR LO MENOS UNA VEZ
+*/
+double estimar_proxima_rafaga(pcb*);
 
 #endif
--- kernel/src/kernelUtils.c
+++ kernel/src/kernelUtils.c
@@ -42,3 +42,12 @@ bool comparar_response_ratio(pcb* un_pcb, pcb* otro_pcb)
 {
     return (calcular_response_ratio(un_pcb) > calcular_response_ratio(otro_pcb));
 }
+
+double estimar_proxima_rafaga(pcb* un_pcb)
+{
+    double tiempo_ejecucion_real = temporal_gettime(un_pcb->tiempo_ejecucion);
+    double alfa = configuracionKernel.HRRN_ALFA;
+    double estimado_anterior = un_pcb->estimado_prox_rafaga;
+    double nuevo_estimado = (alfa * tiempo_ejecucion_real) + ((1 - alfa) * estimado_anterior);
+    return nuevo_estimado;
+}
